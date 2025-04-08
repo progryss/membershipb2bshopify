5a68551f266507d8e9a63676868828a516f0e050
vishal yadav <vishal@progryss.com>
2025-04-08 18:49:49 +0530

    adding omni webhook

--- server/index.js
+++ server/index.js
@@ -4,20 +4,23 @@ const cors = require('cors');
 const app = express();
 const router = require('./router/auth-routes');
 const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
 app.use(express.json());
 app.use(cookieParser());
 
+// CORS setup to allow requests from specified origins and support credentials
 app.use(cors({
     origin: function (origin, callback) {
-        const allowedOrigins = ['https://dashboard.icklebubba.com','https://icklebubba.com','https://icklebubba-test.myshopify.com'];
+        const allowedOrigins = process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : [];
         if (allowedOrigins.includes(origin) || !origin) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    credentials: true,
+    credentials: true, // Allow cookies to be sent with requests
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
 }));
 
 app.use("/api/", router);
